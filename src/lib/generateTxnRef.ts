f2dd40cd443b5250f93090ac1cf858e32bf9905f
Victor Arinde <arindevictor8@gmail.com>
2026-06-16 20:41:51 +0100

    fix: fixed transaction generating utils

--- src/lib/generateTxnRef.ts
+++ src/lib/generateTxnRef.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 
 
-export function generateTxnRef(): string {
+export function generateTxnRef(amount: number): string {
   const myuuid = uuidv4()
   // return `LUXE_${userId}_${Date.now()}`;
   return myuuid;
